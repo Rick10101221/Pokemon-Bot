1bab146da6ac7e8963243d18fb17f278c01512af
Rickesh Khilnani <rikhilna@ucsd.edu>
2020-10-05 01:00:07 -0700

    finished pokemon type processing

--- index.js
+++ index.js
@@ -1,13 +1,179 @@
 const Discord = require('discord.js');
-const client = Discord.Client();
+const client = new Discord.Client();
 
-const tokenFile = require('token.js');
+const tokenFile = require('./token.js');
 const token = tokenFile.token;
 
 client.login(token);
 
+// 18 Types
+let pokemonTypes = [
+    'normal', 
+    'fire', 
+    'water', 
+    'electric', 
+    'grass', 
+    'ice', 
+    'fighting', 
+    'poison', 
+    'ground', 
+    'flying', 
+    'psychic', 
+    'bug', 
+    'rock', 
+    'ghost', 
+    'dragon', 
+    'dark', 
+    'steel', 
+    'fairy'
+];
+
 client.on('message', message => {
+    if (message.author.bot) {
+        return;
+    }
+
     msg = message.content.toLowerCase();
 
-    
+    let splitMsg = msg.split(" ");
+
+    if (splitMsg.length == 1 && splitMsg[0] == "!p") {
+        message.channel.send('------------------------------------------------------------------------------------------------------' +
+            '\nUse me to find the strengths and weaknesses of Pokemon types!\n' + 
+            'Type **`!p type [strengths or weaknesses]`** to get started.\n' + 
+            'Pro tip: `!p type` will automatically display strengths!\n' +
+            '----------------------------------------------------------------------------------------------------------------------------');
+    } else if (splitMsg[0] == "!p") {
+        if (pokemonTypes.includes(splitMsg[1])) {
+            if (splitMsg[2] == 'weaknesses') {
+                message.channel.send('.\n' + findWeaknesses(splitMsg[1]));
+            } else {
+                message.channel.send('.\n'+ findStrengths(splitMsg[1]));
+            }
+        } else {
+            message.channel.send("Format not recognized. Please use `!p type [strengths or weaknesses]`");
+        }
+    }
 });
+
+function findWeaknesses(type) {
+    let text = '';
+    switch(type) {
+        case 'normal':
+            text = 'fighting\n'
+            break;
+        case 'fire':
+            text = 'water\nground\nrock'
+            break;
+        case 'water':
+            text = 'electric\ngrass'
+            break;
+        case 'electric':
+            text = 'ground'
+            break;
+        case 'ice':
+            text = 'fire\nfighting\nrock\nsteel'
+            break;
+        case 'fighting':
+            text = 'flying\npsychic\nfairy' 
+            break;
+        case 'poison':
+            text = 'ground\npsychic'
+            break;
+        case 'ground':
+            text = 'water\ngrass\nice'
+            break;
+        case 'flying':
+            text = 'electric\nice\nrock'
+            break;
+        case 'psychic':
+            text = 'bug\nghost\ndark'
+            break;
+        case 'bug':
+            text = 'fire\nflying\nrock'
+            break;
+        case 'rock':
+            text = 'water\ngrass\nfighting\nground\nsteel'
+            break;
+        case 'ghost':
+            text = 'ghost\ndark'
+            break;
+        case 'dragon':
+            text = 'ice\ndragon\nfairy'
+            break;
+        case 'dark':
+            text = 'fighting\nbug\nfairy'
+            break;
+        case 'steel':
+            text = 'fire\nfighting\nground'
+            break;
+        case 'fairy':
+            text = 'poison\nsteel'
+            break;
+        case 'grass':
+            text = 'fire\nice\npoison\nflying\nbug'
+            break; 
+    }
+    return text;
+}
+
+function findStrengths(type) {
+    let text = '';
+    switch(type) {
+        case 'normal':
+            text = 'none'
+            break;
+        case 'fire':
+            text = 'grass\nice\nbug\nsteel'
+            break;
+        case 'water':
+            text = 'fire\nground\nrock'
+            break;
+        case 'electric':
+            text = 'water\nflying'
+            break;
+        case 'ice':
+            text = 'grass\nground\nflying\ndragon'
+            break;
+        case 'fighting':
+            text = 'normal\nice\nground\nflying\ndragon'
+            break;
+        case 'poison':
+            text = 'grass\nfairy'
+            break;
+        case 'ground':
+            text = 'fire\nelectric\npoison\nrock\nsteel'
+            break;
+        case 'flying':
+            text = 'grass\nfighting\nbug'
+            break;
+        case 'psychic':
+            text = 'fighting\npoison'
+            break;
+        case 'bug':
+            text = 'grass\npsychic\ndark'
+            break;
+        case 'rock':
+            text = 'fire\nice\nflying\nbug'
+            break;
+        case 'ghost':
+            text = 'psychic\nrock'
+            break;
+        case 'dragon':
+            text = 'dragon'
+            break;
+        case 'dark':
+            text = 'psychic\nghost'
+            break;
+        case 'steel':
+            text = 'ice\nrock\nfairy'
+            break;
+        case 'fairy':
+            text = 'fighting\ndragon\ndark'
+            break;
+        case 'grass':
+            text = 'water\nground\nrock'
+            break; 
+    }
+    return text;
+}
