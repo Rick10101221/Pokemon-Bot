64f86c1e586d2538bec9f52de6226871767a17c2
Rickesh Khilnani <rikhilna@ucsd.edu>
2020-10-11 00:24:43 -0700

    restored pokemon info channel and added random index accessing for sprites

--- index.js
+++ index.js
@@ -18,9 +18,8 @@ let pokemonSprites = [];
 
 /*
     TODO: 
-    1. Check if sending a message in a specific channel
-    2. Choose a random index and print the pokemon's name and sprite.
-    3. Figure out how to print 'You caught a level ___ _____! You now have ____ pokemon'
+    1. How to check contents of last message.
+    2. Figure out how to print 'You caught a level ___ _____! You now have ____ pokemon'
     3. Figure out how to create player profiles and add
     caught pokemon to a database.
 */
@@ -58,37 +57,48 @@ client.on('message', async (message) => {
 
     let splitMsg = msg.split(" ");
 
-    if (message.channel.id === catchPokemonChannelId && splitMsg.length == 1 &&
-        splitMsg[0] === "!b" && booted == false) {
-        await fetchUrls();
+    if (message.channel.id === catchPokemonChannelId) {
+        if (msg === '!b' && booted == false) {
+            await fetchUrls();
 
-        for (let i = 0; i < pokemonUrls.length; i++) {
-            console.log('current url', pokemonUrls[i]);
-            pokemonSprites.push(await fetchSprites(pokemonUrls[i]));
-        }
-
-        message.channel.send('done');
-        booted = true;
-        
-    } else if (message.channel.id === pokemonInfoId && splitMsg[0] === '!p') {
-        if (msg === '!p') {
-            message.channel.send('-------------------------------------------------------------------------------------------------------' +
-            '\nUse me to find the strengths and weaknesses of Pokemon types!\n' + 
-            'Type **`!p type [strengths or weaknesses]`** to get started.\n' + 
-            'Pro tip: `!p type` will automatically display strengths!\n' +
-            '------------------------------------------------------------------------------------------------------------------------------');
-        } else if (pokemonTypes.includes(splitMsg[1])) {
-            if (splitMsg[2] == 'weaknesses') {
-                message.channel.send('.\n' + findWeaknesses(splitMsg[1]));
-            } else {
-                message.channel.send('.\n'+ findStrengths(splitMsg[1]));
+            for (let i = 0; i < pokemonUrls.length; i++) {
+                console.log('current url', pokemonUrls[i]);
+                await fetchSprites(pokemonUrls[i]);
             }
-        } else {
-            message.channel.send('Format not recognized. Please use ' + 
-            '`!p type [strengths or weaknesses]`');
+
+            message.channel.send('done');
+            booted = true;
+        } else if (splitMsg[0] === '!catch' && booted == true) {
+            let index = randomNumber(pokemonSprites.length);
+            message.channel.send(pokemonSprites[index]);
+        }
+        
+    } else if (message.channel.id === pokemonInfoId) {
+        if (splitMsg[0] === '!p') {
+            if (msg === '!p') {
+                message.channel.send('-------------------------------------------------------------------------------------------------------' +
+                '\nUse me to find the strengths and weaknesses of Pokemon types!\n' + 
+                'Type **`!p type [strengths or weaknesses]`** to get started.\n' + 
+                'Pro tip: `!p type` will automatically display strengths!\n' +
+                '------------------------------------------------------------------------------------------------------------------------------');
+            } else if (pokemonTypes.includes(splitMsg[1])) {
+                if (splitMsg[2] == 'weaknesses') {
+                    message.channel.send('.\n' + findWeaknesses(splitMsg[1]));
+                } else {
+                    message.channel.send('.\n'+ findStrengths(splitMsg[1]));
+                }
+            } else {
+                message.channel.send('Format not recognized. Please use ' + 
+                '`!p type [strengths or weaknesses]`');
+            }
         }
     }
 });
+
+
+function randomNumber(max) {
+    return Math.floor(Math.random() * max);
+}
 
 
 async function fetchUrls() {
